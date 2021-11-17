6fac9557be2131f3fbf5f120e5fcbf0f0280150d
HoiDam <66233626+HoiDam@users.noreply.github.com>
2021-11-17 16:55:43 +0800

    hot fix document name issue

--- src/database/index.js
+++ src/database/index.js
@@ -3,20 +3,24 @@ let mongoose = require("mongoose")
 let BlockChainModel = require("./model");
 
 let connectionCallback = () => {};
-let doc_code = "A"
+let doc_code
 
-//Connect to DB
-url = "mongodb://localhost:27017/BlockChain_" + doc_code
-console.log(url)
-mongoose.connect(url, (err) => {
-    if(err) 
-        return console.log("Cannot connect to DB");
-    console.log("Database is Connected");
-    connectionCallback();
-});
+function connectDB() {
+    //Connect to DB
+    url = "mongodb://localhost:27017/BlockChain_" + doc_code
+    console.log(url)
+    mongoose.connect(url, (err) => {
+        if(err) 
+            return console.log("Cannot connect to DB");
+        console.log("Database is Connected");
+        connectionCallback();
+    });
+}
+
 
 module.exports.onConnect = (callback,number) => {
     doc_code = number
     connectionCallback = callback;
+    connectDB()
 }
 
